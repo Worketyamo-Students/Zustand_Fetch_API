e3ba996036e87ad6db79c1e0a49c73a7ae4e58bf
melvis8 <melvisnajela@gmail.com>
2024-10-02 19:13:21 +0100

    barthez

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Myline } from './myline.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+const Files = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/myline",
+    element: <Myline />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={Files} />
   </StrictMode>,
 )
